ef049ef4e393bf14e651cb7139d64f0f052420a4
Ma Javeed <mohammedabduljaveed078@gmail.com>
2024-03-10 09:17:54 +0530

    Added addTodo functionality with or without quantity and styled todo items

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
 import React, { useState } from "react";
+import TodoItem from "./components/TodoItem";
 import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [todosInput, setTodosInput] = useState("");
 
-  const handleAddTodo = () => {};
+  const addTodo = (todo) => {
+    setTodos([...todos, todo]);
+    setTodosInput("");
+  };
+
+  const addTodosWithQuantity = (todo, quantity) => {
+    const num = parseInt(quantity);
+    const slicedTodo = todo.slice(0, -1);
+    const tasksArray = [];
+    for (let i = 0; i < num; i++) {
+      tasksArray.push(slicedTodo);
+    }
+    setTodos([...todos, ...tasksArray]);
+    setTodosInput("");
+  };
+
+  const handleAddTodo = (e) => {
+    e.preventDefault();
+    const quantity = todosInput.split(" ").slice(-1)[0];
+    if (isNaN(quantity)) {
+      addTodo(todosInput);
+    } else {
+      addTodosWithQuantity(todosInput, quantity);
+    }
+  };
 
   return (
-    <div className="h-screen bg-gray-700 flex justify-center">
-      <div className="mt-10">
+    <div className="h-screen bg-gray-700 flex flex-col justify-start items-center">
+      <form onSubmit={handleAddTodo} className="mt-10">
         <h1 className="text-center mb-5 font-sans font-medium text-white text-[40px] lg:text-[48px]">
           Daily Goals!
         </h1>
@@ -21,13 +46,13 @@ function App() {
           className="block w-[300px] p-2 outline-none rounded-md pl-3"
         />
         <button
-          onClick={handleAddTodo}
+          type="submit"
           className="bg-gray-500 p-3 text-white rounded-md px-5 mt-1"
         >
           Add todo
         </button>
-      </div>
-      <ul>
+      </form>
+      <ul className="mt-4 min-w-[300px] max-w-full">
         {todos.map((item, index) => (
           <TodoItem key={index} item={item} />
         ))}
--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,14 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { GrEdit } from "react-icons/gr";
+import { RiDeleteBin5Line } from "react-icons/ri";
 
 function TodoItem({ item, index }) {
   const [count, setCount] = useState(0);
   return (
-    <div>
-      <p>
-        {item} <span>(updated {count} times)</span>{" "}
+    <div className="flex gap-10 w-full bg-gray-500 p-3 mb-2 rounded-sm">
+      <p className="text-white font-sans text-[20px]">
+        {item} <span>(updated {count} times)</span>
       </p>
-      <button>edit</button>
-      <button>Delete</button>
+      <div className="flex gap-5">
+        <button>
+          <GrEdit color="white" size={20} />
+        </button>
+        <button>
+          <RiDeleteBin5Line color="red" size={20} />
+        </button>
+      </div>
     </div>
   );
 }
